7f2830eacc332c5f3589ba826fe75f2ef7060f9f
Isabela Mueller <162368740+Mueller07@users.noreply.github.com>
2025-05-06 17:18:49 -0300

    Delete Aula13 directory

--- Aula13/script.js
+++
@@ -1,9 +0,0 @@
-let banners = ["./img/bob.png", "./img/Patrick_Estrela.png"];
-
-let cont = 0;
-function trocaBanner(){
-    cont = (cont +1) % 2;
-    document.querySelector(".destaque img").src = banners[cont];
-}
-
-setInterval(trocaBanner, 1000);
